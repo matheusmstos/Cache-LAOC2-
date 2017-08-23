2262d24c92eac1c5915be39c1dac2654a951938a
Matheus Santos <matheusmstos@yahoo.com.br>
2017-08-23 17:22:24 -0300

    Coloquei o endereço

--- parte3/parte3.v
+++ parte3/parte3.v
@@ -1,17 +1,9 @@
 module conj_ass2vias( input clock,
 							 input write; //0 = leitura 
-							 input[4:0] tag_input, //tag que vem da memoria pra associa com a tag da cache
-							 input index_input);
-							 
-							 
-							 
-							 
-							 
-							 
-							 
-							 
-							 
-							 
+							 input [4:0]adress_input,
+							 input [4:0] bloco_lido_da_memoria //adereco que vem da memoria pra associa com o endereco da cache
+							 input [4:0] bloco_a_ser_escrito_na_cache);
+										 
 		
 	reg[11:0] cache[1:0][1:0]; // conjunto x via/bloco
 	
@@ -20,8 +12,8 @@ module conj_ass2vias( input clock,
 	//lru.       [10]
 	//dirty.     [9]
 	//tag.       [8:5] 
-	//index.     [4] -> meh 
-	//bloco.     [4:0] 
+	//index.     [4]
+	//bloco.     [3:0] 
 	
 	
 	wire valid[1:0];
@@ -31,29 +23,35 @@ module conj_ass2vias( input clock,
 	//wire index[1:0];
 	wire [4:0] bloco_cache[1:0];
 	
-	assign valid[0] = 	 	cache[index_input][0][11];
-	assign valid[1] = 	 	cache[index_input][1][11];
-	assign lru[0] =  		cache[index_input][0][10];
-	assign lru[1] = 		cache[index_input][1][10];
-	assign dirty[0] = 	 	cache[index_input][0][9];
-	assign dirty[1] = 	 	cache[index_input][1][9];
+	//slit do endereco input que vem da memoria
+	wire index_input;
+	wire tag_input;
+	assign index_input = adress_input[0];
+	assign tag_input = adress_input[4:1];
+
+	assign valid[0] = 		cache[index_input][0][11];
+	assign valid[1] = 		cache[index_input][1][11];
+	assign lru[0] =  			cache[index_input][0][10];
+	assign lru[1] = 			cache[index_input][1][10];
+	assign dirty[0] = 	   cache[index_input][0][9];
+	assign dirty[1] = 		cache[index_input][1][9];
 	assign tag_cache[0] = 	cache[index_input][0][8:5];
 	assign tag_cache[1] = 	cache[index_input][1][8:5];
-	//assign index[0] = 	cache[index_input][0][4];
-	//assign index[1] = 	cache[index_input][1][4];
-	assign bloco_cache[0] = cache[index_input][0][4:0];
-	assign bloco_cache[1] = cache[index_input][1][4:0];
+	assign index[0] = 	   cache[index_input][0][4];
+	assign index[1] = 	   cache[index_input][1][4];
+	assign bloco_cache[0] = cache[index_input][0][3:0];
+	assign bloco_cache[1] = cache[index_input][1][3:0];
 	
 	initial begin 
 	
 		//INICIALIZAÇÃO DOS CONJUNTOS 
 	
-		//([11]validade, [10]lru, [9]dirty, [8:5]tag, [4:0]bloco)
-		cache[0][0] = 12'b 1 0 0 0000 00000; //tag = 0000 , valor = 00000(0)
-		cache[0][1] = 12'b 1 0 0 0001 00001; //tag = 0000 , valor = 00001(1)
+		//([11]validade, [10]lru, [9]dirty, [8:5]tag, [4]index, [3:0]bloco)
+		cache[0][0] = 12'b 1 0 0 0000 0 0000; //tag = 0000 , valor = 00000(0)
+		cache[0][1] = 12'b 1 0 0 0001 0 0001; //tag = 0000 , valor = 00001(1)
 		
-		cache[1][0] = 12'b 1 0 0 0010 00010; //tag = 0010 , valor = 00010(2)
-		cache[1][1] = 12'b 1 0 0 0011 00011; //tag = 0011 , valor = 00011(3)
+		cache[1][0] = 12'b 1 0 0 0010 0 0010; //tag = 0010 , valor = 00010(2)
+		cache[1][1] = 12'b 1 0 0 0011 0 0011; //tag = 0011 , valor = 00011(3)
 		
 		//cache[index][via]
 		
